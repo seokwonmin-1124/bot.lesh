17b4d022ec4ecaa5ad02a0a627ce647e2682740d
LESH <71909512+seokwonmin-1124@users.noreply.github.com>
2022-01-01 04:54:38 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,21 +1,21 @@
 #LESH_21.12.18_코로나 크롤링 기능 구현 완료
 #LESH_22.01.01_사용자 프로필 기능 구현 완료
-#아직 파일 나누는 방법을 몰라 한 파일로 만들었습니다.
+#사용 시 출처를 남겨주세요
 import discord
-from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import requests
+from keep_alive import keep_alive
 
 #봇이 하고있는 게임, 상태 설정
 game = discord.Game("LESH와 개발놀이")
 client = discord.Client(status=discord.Status.idle, activity=game)
 
-#코로나 19 웹 크롤링
+#코로나 19 웹 크롤링 함수들
 url = 'http://ncov.mohw.go.kr/bdBoardList_Real.do?brdId=1&brdGubun=13&ncvContSeq=&contSeq=&board_id=&gubun=' #이 링크에서 크롤링
 res = requests.get(url)
 res.raise_for_status()
 
-soup = BeautifulSoup(res.text, 'lxml') #pip install lxml
+soup = BeautifulSoup(res.text, 'lxml')
 covid19_all = soup.select_one('#content > div > div.data_table.midd.mgt24 > table > tbody > tr.sumline > td:nth-child(5)').text #전체 확진자 수
 covid19_today = soup.select_one('#mapAll > div > ul > li:nth-child(6) > div:nth-child(2) > span').text #오늘 확진자 수
 covid19_die = soup.select_one('#mapAll > div > ul > li:nth-child(1) > div:nth-child(2) > span').text #사망자 수
@@ -33,39 +33,40 @@ async def on_message(message):
         return
 
     if message.content.startswith('.코로나'): #봇이 받은 메시지가 .코로나로 시작하면
-        embed=discord.Embed(title="코로나 확진자 정보", color=0x8ce137) #임베드 생성
+        embed=discord.Embed(title="코로나 확진자 정보", color=0x8ce137) #임베드 에브리드 생성
         embed.set_thumbnail(url="https://yt3.ggpht.com/ytc/AKedOLRx1o4FfsK5isI9U-EHzAt7S57Knoyv7MoEIGKpGw=s900-c-k-c0x00ffffff-no-rj" ) #썸네일 설정
         embed.add_field(name="총 확진자", value=f'{covid19_all}명{covid19_today}', inline=True) #임베드에 필드 추가
         embed.add_field(name="총 사망자", value=f'{covid19_die}명{covid19_die_plus}', inline=True) #임베드에 필드 추가
-        embed.set_footer(text="bot.lesh_확진자 정보" ) #임베드에 푸터 추가
+        embed.set_footer(text=f'{covid19_date}시 기준 확진자 정보입니다' ) #임베드에 푸터 추가
         await message.channel.send(embed=embed) #메시지 전송
-     
-    if message.content.startswith('.uesr embed'):
-        uesr_name = message.author.name
+    
+    if message.content.startswith('.내정보'):
+        #user_name = message.author.name
         user_server_name = message.author.display_name
-        user_tag = message.author.discriminator
-        #name_with_tag = message.author
-        user_id = message.author.id
-        uesr_start_discord_date= message.author.created_at
+        #user_tag = message.author.discriminator
+        user_name_with_tag = message.author
+        #user_id = message.author.id
+        user_start_discord_date= message.author.created_at
         user_profile_img = message.author.avatar_url
-        uesr_mention = message.author.mention
+        user_mention = message.author.mention
 
         await message.channel.send(f'bot.lesh가 알려주는 {user_mention} 님의 정보입니다!')
-        embed=discord.Embed(title=f'{uesr_name}님의 정보', color=0x8ce137)
-        embed.add_field(name='이름', value=user_name, inline=True)
-        embed.add_field(name='서버에서의 이름', value=uesr_server_name, inline=True)
-        embed.add_field(name='디스코드를 시작한 날', value=user_start_discord.strftime('%Y-%m-%d'), inline=True)
+        embed=discord.Embed(title=f'{user_server_name}님의 정보', color=0x8ce137)
+        embed.add_field(name='고유 이름', value=user_name_with_tag, inline=True)
+        embed.add_field(name='서버 별명', value=user_server_name, inline=True)
+        embed.add_field(name='디스코드를 시작한 날',  value=user_start_discord_date.strftime('%Y-%m-%d'), inline=True)
         embed.set_thumbnail(url=user_profile_img)
-        embed.set_footer(text=f'{uesr_name}님의 개인 프로필' )
+        embed.set_footer(text='.봇정보 으로 봇의 정보도 알아보세요' )
         await message.channel.send(embed=embed)
 
     if message.content.startswith('.핑'): #봇이 받은 메시지가 .핑으로 시작하면
-        await message.channel.send(f'``{client.latency * 1000:.0f}ms``') #봇의 지연시간(핑)을 출력
+        await message.channel.send(f'`{client.latency * 1000:.0f}ms`') #봇의 지연시간(핑)을 출력
     
     if message.content.startswith(".비웃어"): #봇이 받은 메시지가 .비웃어로 시작하면
-        await message.channel.send("```ㅋ``") #봇이 ㅋ을 출력
+        await message.channel.send("`ㅋ`") #봇이 ㅋ을 출력
 
     
 
+keep_alive()
+client.run('OTIxMjg4NDQzODY5Mzk2OTky.YbwuuQ.nIzfkH2JKBqu3xLu9sfsTZNyGEo') #봇이 실행되면 토큰 파일을 읽어서 봇이 로그인함
 
-client.run('토큰입력')
