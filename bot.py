b840000b14b89638fe88f2c298b4091e5e9853a3
LESH <71909512+seokwonmin-1124@users.noreply.github.com>
2022-01-07 09:26:01 +0900

    hex 기능 오류 수정, 초대 기능 추가🎉

--- bot.py
+++ bot.py
@@ -3,7 +3,6 @@
 #LESH_22.01.01_사용자 프로필 기능 구현 완료
 #LESH_22.01.02_HEX 코드 뷰어 가눙 구현 완료
 #사용 시 출처를 남겨주세요
-
 #-------모듈 import--------#
 import discord
 from bs4 import BeautifulSoup
@@ -11,7 +10,7 @@ import requests
 from discord.ext import commands
 
 #--------봇 상태 설정--------#
-game = discord.Game("LESH와 개발놀이")
+game = discord.Game(".도움말")
 bot = commands.Bot(command_prefix='.', status=discord.Status.idle, activity=game)#봇이 하고있는 게임(~하는중), 상태 설정
 
 #--------초기세팅--------#
@@ -75,18 +74,41 @@ async def 내정보(ctx):
     embed.set_footer(text='.봇정보 으로 봇의 정보도 알아보세요' )
     await ctx.channel.send(embed=embed)
 
-@bot.command() #HEX
+@bot.command()
 async def hex(ctx, hex_number_with_hashtag):
-    hex_number = hex_number_with_hashtag[1:7]
-    #hex_number_with_hashtag.replace('#', '')
+    hex_number = hex_number_with_hashtag[1:7]#hex_number_with_hashtag.replace('#', '')
     url = f"https://www.colorhexa.com/{hex_number}"
     hex_img = f"{url}.png"
 
-    embed=discord.Embed(title=f'Hex code | {hex_number_with_hashtag}', color=0x8ce137)
-    embed.add_field(name="설명", value=f'{hex_number_with_hashtag}에 해당하는 색상입니다', inline=False)
-    embed.set_thumbnail(url=hex_img)
-    embed.set_footer(text="image by colorhexa.com")
-    await ctx.send(embed=embed)
+    #만약 함수 {hex_code}의 길이가 6이라면 헥스 코드에 맞는 이미지를 출력하고 그렇지 않다면 에러를 출력한다
+    if len(hex_number) == 6: #만약 함수 hex_number의 길이가 6이라면 아래 내용 실행/전송
+
+        embed=discord.Embed(title=f'Hex code | {hex_number_with_hashtag}', color=0x8ce137)
+        embed.add_field(name="설명", value=f'{hex_number_with_hashtag}에 해당하는 색상입니다', inline=False)
+        embed.set_thumbnail(url=hex_img)
+        embed.set_footer(text="image by colorhexa.com")
+        await ctx.send(embed=embed)
+        
+    else: #그렇지 않다면 에러 임베드 전송
+
+        embed = discord.Embed(title="잘못된 입력입니다.", description="올바른 입력은 #으로 시작하는 6자리의 숫자 또는 문자 조합입니다.", color=0xff0000)
+        await ctx.send(embed=embed)
+
+@bot.command() #도움말
+async def 도움말(ctx):
+    embed=discord.Embed(title="bot.lesh의 도움말 입니다", color=0x8ce137)
+    embed.add_field(name="메인 명령어", value="`.도움말`: bot.lesh의 도움말을 알려줍니다.\n`.코로나`: 하루마다 업데이트 되는 코로나 확진자 수를 알려줍니다\n`.내정보`: 내 서버별명, 고유이름, 디스코드를 시작한 날짜, 프로필 사진 등을 알려줍니다.\n`.hex #(hex_code)`: hex 코드를 이미지로 보여줍니다.\n", inline=False)
+    embed.add_field(name="잡기능들", value="`.비웃어`: 봇이 `ㅋ`하고 비웃어 줍니다.\n`.핑`:핑 상태를 알려줍니다", inline=False)
+    embed.add_field(name="봇 초대 방법", value="`.초대` 를 이용하여 봇을 초대해 보세요!\n 봇 인스타그램에도 초대 주소가 있습니다`instagram`:bot.lesh", inline=False)
+    embed.add_field(name="개발자 정보", value="07년생 중등 개발자 입니다.\n`Didcord`:LESH#3201\n`Blog`: blog.naver.com/seokwonmin\n`instagram`: LESH_1124", inline=False)
+    embed.set_footer(text="bot.lesh")
+    await ctx.channel.send(embed=embed)
+
+@bot.command()
+async def 초대(ctx):
+    embeds = discord.Embed(title="bot.lesh를 서버에 초대하세요!", color=0x8ce137)
+    embeds.add_field(name="링크입니다", value="[지금 바로 초대하세요](https://discord.com/api/oauth2/authorize?client_id=921288443869396992&permissions=380104624192&scope=bot)")
+    await ctx.send(embed=embeds)
 
 #--------토큰--------#
-bot.run('토큰입력') #봇이 실행되면 토큰 파일을 읽어서 봇이 로그인함
+bot.run(InputYourBotToken) #봇이 실행되면 토큰 파일을 읽어서 봇이 로그인함
